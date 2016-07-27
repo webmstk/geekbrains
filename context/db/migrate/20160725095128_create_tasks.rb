f279bf6eabe72c85c6258e1d6c5cf755c3f696c1
webmstk <webmstk@mail.ru>
2016-07-27 15:45:31 +0300

    add default values to tasks migration

--- context/db/migrate/20160725095128_create_tasks.rb
+++ context/db/migrate/20160725095128_create_tasks.rb
@@ -1,10 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.integer :progress
+      t.integer :progress,  default: 0
       t.string  :content
-      t.boolean :completed
-      t.boolean :context
+      t.boolean :completed, default: false
+      t.boolean :context,   default: false
 
       t.timestamps
     end
--- context/db/schema.rb
+++ context/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713185700) do
+ActiveRecord::Schema.define(version: 20160725095128) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "progress",   default: 0
+    t.string   "content"
+    t.boolean  "completed",  default: false
+    t.boolean  "context",    default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "hashed_password", null: false
